f63214e1c0649162d9afdf34eeeb305eb665335c
Syd Vicious <cjschwichtenberg@gmail.com>
2018-07-31 02:24:37 -0400

    updating bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "passphrase",
+    password: "XXXXXXXXX",
     database: "bamazonDB"
 });
 
